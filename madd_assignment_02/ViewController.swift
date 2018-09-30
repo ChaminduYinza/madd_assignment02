ac6d5312da619bcf722088e8e09d0cc38bb9931e
Chamindu Thiranjaya <chaminduthiranjaya@chamindus-mac.local>
2018-09-30 22:27:06 +0530

    added search button and functionalities

--- madd_assignment_02/ViewController.swift
+++ madd_assignment_02/ViewController.swift
@@ -9,37 +9,39 @@
 import UIKit
 
 
-class ViewController: UIViewController, UITableViewDataSource{
+class ViewController: UIViewController, UITableViewDataSource, UISearchBarDelegate{
     
-    let urlString = "https://itunes.apple.com/search?term=Puzzle&limit=200&entity=software"
     var listData:[ListItem] = []
-    
+    @IBOutlet weak var searchBar: UISearchBar!
     @IBOutlet weak var tableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        callAPI()
+        searchBar.delegate = self
     }
     
     /*
      * Call API function
      */
-    func callAPI() {
-        let url = NSURL(string: urlString)
+    func callAPI(paramURL:String) {
+        print(paramURL)
+        let url = NSURL(string: paramURL)
         //Send get request to the given URL
-        URLSession.shared.dataTask(with: (url as? URL)!, completionHandler: {(data, response, error) -> Void in
+        URLSession.shared.dataTask(with: (url as URL?)!, completionHandler: {(data, response, error) -> Void in
             //Converting response to readable JSON
             if let jsonObj = try? JSONSerialization.jsonObject(with: data!, options: .allowFragments) as? NSDictionary {
                 //Validate whether the reponse has a key named 'results'
                 if let resultSet = jsonObj!.value(forKey: "results") as? NSArray {
+                    //Clear current search history
+                    self.listData.removeAll()
                     //Loop through each element in results array in response recieved from the API
                     for element in resultSet {
                         //Convert curent element into a NSDictionary
                         if let elementDict = element as? NSDictionary {
                             //Valdiate whehter the current element has a key named 'trackName'
                             if let title = elementDict.value(forKey: "trackName"){
-                                 //Valdiate whehter the current element has a key named 'artistName'
+                                //Valdiate whehter the current element has a key named 'artistName'
                                 if let artistName = elementDict.value(forKey: "artistName"){
-                                     //Valdiate whehter the current element has a key named 'artworkUrl60'
+                                    //Valdiate whehter the current element has a key named 'artworkUrl60'
                                     if let imageURL = elementDict.value(forKey: "artworkUrl60"){
                                         //Append data into the ListItem
                                         self.listData.append(ListItem(title: title as! String, artistName: artistName as! String, imageURL: imageURL as! String))
@@ -68,9 +70,22 @@ class ViewController: UIViewController, UITableViewDataSource{
         cell.lblTitle.text = listData[indexPath.row].title
         cell.lblArtistName.text = listData[indexPath.row].artistName
         let imgURL = NSURL(string: listData[indexPath.row].imageURL)
-        let data = NSData(contentsOf: (imgURL as? URL)!)
-        cell.imgLogo.image = UIImage(data: data as! Data)
+        let data = NSData(contentsOf: (imgURL as URL?)!)
+        cell.imgLogo.image = UIImage(data: data! as Data)
         return cell
     }
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        let searchText = self.searchBar.text
+        //Concat typed string with URL
+        let urlString = "https://itunes.apple.com/search?term="+searchText!+"&init=200&entity=software"
+        //Encode url string
+        let encodedString = urlString.addingPercentEncoding(withAllowedCharacters: CharacterSet.urlQueryAllowed)
+        guard !(searchText?.isEmpty)! else {
+            return
+        }
+        self.callAPI(paramURL: encodedString!)
+    }
 }
+
 
