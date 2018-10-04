b5b197dc0a6e5224dfac162b01b84c7ad51b01ce
Rushan Edirisuriya <rushanedirisuriya@chamindus-mac.local>
2018-10-04 11:42:37 +0530

    create popup UI view controller and linked with popup view controller

---
+++ madd_assignment_02/PopupVCViewController.swift
@@ -0,0 +1,27 @@
+//
+//  PopupVCViewController.swift
+//  madd_assignment_02
+//
+//  Created by Rushan Edirisuriya on 10/4/18.
+//  Copyright © 2018 Chamindu Thiranjaya. All rights reserved.
+//
+
+import UIKit
+
+class PopupVCViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+
+
+}
