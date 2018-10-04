6b71caaf08899ba0db3a46566cbedab9f86cffff
Rushan Edirisuriya <rushanedirisuriya@chamindus-mac.local>
2018-10-04 11:52:59 +0530

    Create Storyboard segue connection between view controller and popupvc with identifier called PopUpVC

---
+++ madd_assignment_02/PopupVC.swift
@@ -0,0 +1,13 @@
+//
+//  PopupVC.swift
+//  madd_assignment_02
+//
+//  Created by Rushan Edirisuriya on 10/4/18.
+//  Copyright © 2018 Chamindu Thiranjaya. All rights reserved.
+//
+
+import UIKit
+
+class PopupVC: NSObject {
+
+}
